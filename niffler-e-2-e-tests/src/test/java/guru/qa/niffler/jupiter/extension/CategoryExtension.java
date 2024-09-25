05c2ff92d9482b5744ecea8c13e128168ffe08b0
Artem Chubukov <achubukov@jt-soft.ru>
2024-09-25 16:37:40 +0300

    hw 3.3 refactoring afterTestExecution

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -58,7 +58,7 @@ public class CategoryExtension implements BeforeEachCallback, ParameterResolver,
     @Override
     public void afterTestExecution(ExtensionContext context) throws Exception {
       CategoryJson category = context.getStore(NAMESPACE).get(context.getUniqueId(), CategoryJson.class);
-      if (!category.archived()) {
+      if (category != null && !category.archived()) {
           category = new CategoryJson(
                   category.id(),
                   category.name(),
